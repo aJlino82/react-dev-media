b36efc02bc3b8e97133e72b0826b597ff8508fe8
Adailton Junior <adailton_jr@hotmail.com>
2022-09-23 16:24:20 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>React JS</header>
+
+      <main>
+        <section className="box-titulo">
+          <h1>React JS</h1>
+          <p>JavaScipt para SPAs</p>
+          <a href='https://reactjs.org/' target="_blank" rel="noreferrer" >Site Oficial</a>
+        </section>
+
+        <section className="box-requsitos">
+          <h2>Requisitos</h2>
+          <p>Requisitos mínimos para usar React</p>
+
+
+          <div className="box-cards">
+            <div className="card">
+              <h3>HTML</h3>
+              <p>Apredender a estuturar as elementos da página com HTML</p>
+            </div>
+            <div className="card">
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+            <div className="card">
+              <h3>JavaScript</h3>
+              <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p className="autor">Curso de introdução de React JS</p>
+      </footer>
     </div>
   );
 }
